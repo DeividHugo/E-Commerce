d64b32306b71cbdf5787b831228f7f39500b67aa
Deivid Hugo <deividhugoof@gmail.com>
2022-11-10 23:13:17 -0300

    anotherBranch

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
 from .views import index, loginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('login/', loginView, name='login'),
 
 ]
