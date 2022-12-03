bbd939c732295adad3d01f2ba04942b4ee110016
Deivid Hugo <deividhugoof@gmail.com>
2022-12-03 10:48:44 -0300

    Merge

--- project/apps/depot/models.py
+++ project/apps/depot/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 
 # Create your models here.
 class Product(models.Model):
@@ -11,3 +11,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Client(models.Model):
+    name = models.CharField(max_length=80)
+    image = models.ImageField(upload_to='static/media/users/', height_field=None, width_field=None)
+    address = models.CharField(max_length=280)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class ClintHasProduct(models.Model):
+    # client = 
+    # product = 
